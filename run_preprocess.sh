4ac9a217a4f88d4a1a5365bfd166cf694de6b3ac
Celso <c.el@live.com>
2023-02-22 09:51:30 -0300

    feat: add helpers.

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,18 +1,15 @@
-### Preprocess
-#python preprocess.py \
-#--text-path data/Wiki10-31k/train_texts.txt \
-#--label-path data/Wiki10-31k/train_labels.txt \
-#--vocab-path data/Wiki10-31k/vocab.npy \
-#--emb-path data/Wiki10-31k/emb_init.npy \
-#--w2v-model data/glove.840B.300d.gensim
-#
-#python preprocess.py \
-#--text-path data/Wiki10-31k/test_texts.txt \
-#--label-path data/Wiki10-31k/test_labels.txt \
-#--vocab-path data/Wiki10-31k/vocab.npy
+# activate venv and set Python path
+source ~/projects/venvs/AttentionXML/bin/activate
+export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
+## Preprocess
+python preprocess.py \
+--text-path data/Wiki10-31k/train_texts.txt \
+--label-path data/Wiki10-31k/train_labels.txt \
+--vocab-path data/Wiki10-31k/vocab.npy \
+--emb-path data/Wiki10-31k/emb_init.npy \
+--w2v-model data/glove.840B.300d.gensim
 
-
-# Train and predict as follows:
-python main.py \
-  --data-cnf configure/datasets/Wiki10-31k.yaml \
-  --model-cnf configure/models/AttentionXML-Wiki10-31k.yaml
+python preprocess.py \
+--text-path data/Wiki10-31k/test_texts.txt \
+--label-path data/Wiki10-31k/test_labels.txt \
+--vocab-path data/Wiki10-31k/vocab.npy
