f5b30a1526a8e3c7a7e0550b75a870f77003ac31
Celso <c.el@live.com>
2023-02-27 11:15:57 -0300

    feat: ready to first fly.

--- scripts/run_xml.sh
+++ scripts/run_xml.sh
@@ -3,6 +3,6 @@
 python main.py --data-cnf setting/data/$1.yaml --model-cnf setting/model/$2-$1.yaml -t 0
 python main.py --data-cnf setting/data/$1.yaml --model-cnf setting/model/$2-$1.yaml -t 1
 python main.py --data-cnf setting/data/$1.yaml --model-cnf setting/model/$2-$1.yaml -t 2
-python ensemble.py -p resource/result/$2-$1 -t 3
+python ensemble.py -p resource/prediction/$2-$1 -t 3
 
 
