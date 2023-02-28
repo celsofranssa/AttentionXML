1f8dc4cbd9964e102da03d0ce20778667ec9b47a
Celso <c.el@live.com>
2023-02-28 14:22:38 -0300

    feat: ready to first fly.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-source ~/projects/venvs/AttentionXML/bin/activate
-export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
+#source ~/projects/venvs/AttentionXML/bin/activate
+#export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
 #export LC_ALL=C.UTF-8
 #export LANG=C.UTF-8
 
 DATA=Eurlex-4k
 MODEL=AttentionXML
-FOLD=3
+FOLD=0
 
 bash scripts/run_preprocess.sh $DATA $FOLD
 bash scripts/run_xml.sh $DATA $MODEL $FOLD
