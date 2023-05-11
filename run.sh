094aaf060ed556878cd7d0aa89aab860df4511f4
Celso <c.el@live.com>
2023-05-10 22:46:40 -0300

    feat: add run.sh to Eurlex-4k.

--- run.sh
+++ run.sh
@@ -1,19 +1,22 @@
-#source ~/projects/venvs/AttentionXML/bin/activate
-#export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
+source ~/projects/venvs/AttentionXML/bin/activate
+export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
 
-DATA=Wiki10-31k
+DATA=Eurlex-4k
 MODEL=AttentionXML
 FOLD=0
-train_start=$(date)
+
+time_start=$(date '+%Y-%m-%d %H:%M:%S')
 bash scripts/run_preprocess.sh $DATA $FOLD
+time_end=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$time_start,$time_end" > preprocess_time.txt
+
+time_start=$(date '+%Y-%m-%d %H:%M:%S')
 bash scripts/run_xml.sh $DATA $MODEL $FOLD
-train_end=$(date)
+time_end=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$time_start,$time_end" > fit_time.txt
 
-predict_start=$(date)
-python ensemble.py -d $1 -m $2 -f $3 -p resource/prediction/$2-$1 -r resource/prediction -t 3
-predict_end=$(date)
-
-
-echo "Training Started at $train_start and ended at $train_end"
-echo "Prediction Started at $predict_start and ended at $predict_end"
+time_start=$(date '+%Y-%m-%d %H:%M:%S')
+python ensemble.py -d $DATA -m $MODEL -f $FOLD -p resource/prediction/$MODEL-$DATA -r resource/prediction -t 3
+time_end=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$time_start,$time_end" > predict.txt
 
--- run_alt.sh
+++ run_alt.sh
@@ -1,10 +1,19 @@
-source ~/projects/venvs/AttentionXML/bin/activate
-export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
+#source ~/projects/venvs/AttentionXML/bin/activate
+#export PYTHONPATH=$PATHONPATH:~/projects/AttentionXML/
 
 DATA=Amazon-670k
 MODEL=FastAttentionXML
-FOLD=2
-
+FOLD=0
+train_start=$(date)
 bash scripts/run_preprocess.sh $DATA $FOLD
 bash scripts/run_xml.sh $DATA $MODEL $FOLD
+train_end=$(date)
+
+predict_start=$(date)
+python ensemble.py -d $1 -m $2 -f $3 -p resource/prediction/$2-$1 -r resource/prediction -t 3
+predict_end=$(date)
+
+
+echo "Training Started at $train_start and ended at $train_end"
+echo "Prediction Started at $predict_start and ended at $predict_end"
 
